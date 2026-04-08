3f6e8ba94a835047af427cd62d37ea8f19eaac47
Thomas Gosling <tomgosling57@gmail.com>
2026-04-08 13:15:21 +1000

    added constants.py file with constant variables

---
+++ snc/snc/constants.py
@@ -0,0 +1,19 @@
+hazard_names_map = {
+    "Unknown": 0,
+    "Explosive": 1,
+    "Flammable Gas": 2,
+    "Non-Flammable Gas": 3,
+    "Dangerous When Wet": 4,
+    "Flammable Solid": 5,
+    "Spontaneously Combustible": 6,
+    "Oxidizer": 7,
+    "Organic Peroxide": 8,
+    "Inhalation Hazard": 9,
+    "Poison": 10,
+    "Radioactive": 11,
+    "Corrosive": 12
+}
+
+HAZARD_SIGNAL_TOPIC = '/snc/hazard_signal'
+ROBOT_POSE_TOPIC = '/snc/robot_pose'
+RETURN_WAYPOINTS_TOPIC = '/snc/return_waypoints'
