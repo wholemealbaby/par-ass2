a7659f5ad7d992185e093be08f9b8dee0ad05e8b
Thomas Gosling <tomgosling57@gmail.com>
2026-04-09 14:47:24 +1000

    added weight for transform logic to start of main loop in navigation node

--- snc/snc/navigation_node.py
+++ snc/snc/navigation_node.py
@@ -29,6 +29,30 @@ def main():
     node.destroy_node()
     rclpy.shutdown()
 
+def main(args=None):
+    rclpy.init(args=args)
+    node = PathTracingNode()
+
+    # Pre-flight check
+    # Wait max 10 secs for the robot to appear in the TF tree
+    node.get_logger().info("Performing pre-flight TF check...")
+    ready = node.tf_buffer.wait_for_transform_async('map', 'base_link', rclpy.time.Time())
+    
+    # Keep node responsive while waiting for transform
+    # Abort on keyboard interrupt
+    try:
+        rclpy.spin_until_future_complete(node, ready, timeout_sec=10.0)
+
+        if ready.done():
+            node.get_logger().info("Robot pose detected. Starting recorder.")
+            rclpy.spin(node)
+        else:
+            node.get_logger().error("Timeout: Could not find robot pose. Is the remote robot running?")
+    except KeyboardInterrupt:
+        pass
+        
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
