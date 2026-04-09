c300f6b913835867eed7356f267d3d81cae9aaba
Thomas Gosling <tomgosling57@gmail.com>
2026-04-09 11:39:53 +1000

    added transform listener and buffer and config parameter class members to the navigation node initialization method

--- snc/snc/navigation_node.py
+++ snc/snc/navigation_node.py
@@ -4,6 +4,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Empty
 from nav_msgs.msg import Path
+from tf2_ros import TransformListener, Buffer
 from constants import (
     PATH_EXPLORE_BUFFER_SIZE,
     PATH_RETURN_BUFFER_SIZE,
@@ -12,14 +13,20 @@ from constants import (
     PATH_EXPLORE_TOPIC,
     PATH_RETURN_TOPIC
 )
+
 class PathTracingNode(Node):
     def __init__(self):
         super().__init__('path_tracing_node')
         self.get_logger().info('Path tracing node launched')
-        # Add a timer to show it's alive
-        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.pose_sample_interval_s = self.get_parameter('pose_sample_interval_s').get_parameter_value().double_value
+        self.waypoint_spacing_min = self.get_parameter('waypoint_spacing_min').get_parameter_value().double_value
+
+        self.sample_pose_timer = self.create_timer(self.pose_sample_interval_s, self.sample_pose_callback)
         self.counter = 0
         
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
         # Return home trigger
         self.sub_home_trigger = self.create_subscription(
             Empty,
