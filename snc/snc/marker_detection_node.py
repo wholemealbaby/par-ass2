db271d0ed30cf978c8f7f8b18ae3d99c7c7cf111
Thomas Gosling <tomgosling57@gmail.com>
2026-04-14 15:21:49 +1000

    added imports for start challenge topic, office and interface

--- snc/snc/marker_detection_node.py
+++ snc/snc/marker_detection_node.py
@@ -2,19 +2,26 @@
 
 import rclpy
 from rclpy.node import Node
-
+from constants import (
+    START_CHALLENGE_INTERFACE,
+    START_CHALLENGE_TOPIC,
+    START_CHALLENGE_BUFFER_SIZE,
+)
 
 class MarkerDetectionNode(Node):
     def __init__(self):
         super().__init__('marker_detection_node')
         self.get_logger().info('Marker detection node launched')
-
+        
+        
         # Create publisher to /snc_start topic
         self.pub_start_challenge(
             START_CHALLENGE_INTERFACE,
             START_CHALLENGE_TOPIC,
             START_CHALLENGE_BUFFER_SIZE,
-        ) 
+        )
+
+
     def timer_callback(self):
         pass
         
