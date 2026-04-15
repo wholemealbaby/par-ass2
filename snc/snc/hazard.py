00b64484de4738dd2587f662dec85ec41b82aa1e
Thomas Gosling <tomgosling57@gmail.com>
2026-04-15 14:29:07 +1000

    fixed typo from previous commit

--- snc/snc/hazard.py
+++ snc/snc/hazard.py
@@ -42,9 +42,9 @@ class HazardManager:
         for obj in detected_objects:
             # Create Hazard from the abstract Object data
             # Assuming Hazard inherits from or wraps Object
-            hazard = Hazard(obj) 
+            hazard = Hazard(obj, obj.header) 
             
-            self.list.append(hazard, obj.header)
+            self.list.append(hazard, )
             new_names.add(hazard.name)
 
         # Update set and metadata
